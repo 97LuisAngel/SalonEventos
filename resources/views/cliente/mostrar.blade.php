3185f25c844fa17669bb0ab22e7e66cadce7347f
97LuisAngel <reyes_angel.97@hotmail.com>
2019-11-20 19:33:40 -0600

    mostrar add

--- resources/views/cliente/mostrar.blade.php
+++ resources/views/cliente/mostrar.blade.php
@@ -2,6 +2,18 @@
 @section('content')
 <div class="row">
     <div class="container">
+        {{-- Nav-Bar-MenúDashboars --}}
+        <ul class="nav justify-content-center">
+                <li class="nav-item">
+                  <a class="nav-link active" href="{{ url('cliente/inicio') }}">Inicio</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="{{ url('/paquetes') }}" tabindex="-1" aria-disabled="true">Paquetes</a>
+                </li>
+              </ul>
+        {{-- END-NavBar --}}
+
+
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2"> Evento</h1>
                         <div class="btn-toolbar mb-2 mb-md-0">
@@ -32,14 +44,14 @@
                     <tbody>
                       <tr>
                         <th scope="row">
-                                <div class="col-md-10">
+                                <div class="col-md-12">
                                         <div class="card mb-4 shadow-sm">
                                           <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"></svg>
                                           <div class="card-body">
                                             <p class="card-text">FOTO 1</p>
                                             <div class="btn-group">
-                                                    <button type="button" class="btn btn-sm btn-outline-secondary">Actualizar <input type="file" name="" id=""></button>
-                                                    <button type="button" class="btn btn-sm btn-outline-secondary">Eliminar</button>
+                                                    <button type="button" class="btn btn-primary">Actualizar <input type="file" name="" id=""></button>
+                                                    <button type="button" class="btn btn-secondary">Eliminar</button>
                                                   </div>
                                             <div class="d-flex justify-content-between align-items-center">
                                               <small class="text-muted">9 mins</small>
@@ -49,14 +61,14 @@
                                 </div>
                         </th>
                         <td>
-                                <div class="col-md-10">
+                                <div class="col-md-12">
                                         <div class="card mb-4 shadow-sm">
                                           <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"></svg>
                                           <div class="card-body">
                                             <p class="card-text">FOTO 1</p>
                                             <div class="btn-group">
-                                                    <button type="button" class="btn btn-sm btn-outline-secondary"><input type="file" name="" id=""></button>
-                                                    <button type="button" class="btn btn-sm btn-outline-secondary">Eliminar</button>
+                                                    <button type="button" class="btn btn-primary">Actualizar <input type="file" name="" id=""></button>
+                                                    <button type="button" class="btn btn-secondary">Eliminar</button>
                                                   </div>
                                             <div class="d-flex justify-content-between align-items-center">
                                               <small class="text-muted">9 mins</small>
@@ -65,25 +77,6 @@
                                         </div>
                                 </div>
                         </td>
-                        <td>
-                                <div class="col-md-10">
-                                        <div class="card mb-4 shadow-sm">
-                                          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"></svg>
-                                          <div class="card-body">
-                                            <p class="card-text">FOTO 1</p>
-                                            <div class="btn-group">
-                                                    <button type="button" class="btn btn-sm btn-outline-secondary">Actualizar <input type="file" name="" id=""></button>
-                                                    <button type="button" class="btn btn-sm btn-outline-secondary">Eliminar</button>
-                                                  </div>
-                                            <div class="d-flex justify-content-between align-items-center">
-                                              <small class="text-muted">9 mins</small>
-                                            </div>
-                                          </div>
-                                        </div>
-                                </div>
-
-                        </td>
-
                       </tr>
 
                     </tbody>
